f427739225a27bebd512935dfdd30e283a27af21
Grace Durant <gdurant34@gmail.com>
2022-09-20 00:58:55 -0700

    commit tree 66296f0cc35a5450d64f405c04a4bfcc1309ff23

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const newHeader = document.createElement("h1");
+
+const main = document.getElementById("main");
+main.remove();
+
+newHeader.setAttribute('id', 'victory');
+
+
+newHeader.textContent = "Grace Durant is the champion";
+document.body.append(newHeader);
